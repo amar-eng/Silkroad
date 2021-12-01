4fba4b3d72d38e9d287143f57d0175db2d7bf0d9
amar-eng <am24mo@gmail.com>
2021-11-30 19:02:24 -0500

    hero section completed

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -4,7 +4,13 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero__content">
-        <h1 className="hero__title">Silkroad</h1>
+        <h1 className="hero__title">Suad silkroad</h1>
+        <p className="hero__subtitle">
+          Re-introducing Aromatic resins and exotic incense{" "}
+        </p>
+      </div>
+      <div className="hero__button">
+        <div className="hero__button-text">Explore</div>
       </div>
     </div>
   );
