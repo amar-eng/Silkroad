2c83ebb699d7b35c0eedbb96b92c4371a27a8540
amar-eng <am24mo@gmail.com>
2021-12-05 23:07:33 -0500

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TurkishHome from "./pages/TurkishHome";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Router>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/turkish" component={TurkishHome} />
       </Switch>
-    </>
+    </Router>
   );
 }
 
